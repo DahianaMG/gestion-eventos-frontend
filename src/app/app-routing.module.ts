ff70e6fa05d7538e0d382ace9cdc534c21ec3221
DahianaMG <122055480+DahianaMG@users.noreply.github.com>
2025-08-04 17:11:27 -0300

    Configuracion inicial con Bootstrap, CORS, environments y modulo HttpClient

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*{ path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'events', component: HomeComponent },
+  { path: 'event/:id', component: AdoptarComponent },
+  { path: 'register', component: AboutComponent },
+  { path: 'login', component: ContactoComponent },
+  { path: '**', component: EventsComponent }*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.anime-events.com/api', //placeholder
+};
